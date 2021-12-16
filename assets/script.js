6dd9786f3ba8b4e64fbe0f3d0a8ce10cf68e0542
Rhea M <rheamalviya@rheas-air-2.myfiosgateway.com>
2021-12-16 12:59:18 -0500

    pseudocode

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+
+// question array on seperate js file
+//questions are objects in array
+//every object has the values for title, choices, answer
+//global variables at top
+//click start to begin quiz
+//start button takes you to first question
+//first question loads with choices
+//timer begins ticking down
+//when you click a choice, feedback is given, and you move to next question
+//if choice is wrong then lose 10 seconds on timer
+//once there's no more questions or no more time, timer stops and you go to end page
+//end page appears
+//input initials with correct criteria
+//press enter/click submit
+//score is saved to highscores
+//then go to highscore page
+//you can clear highscore, share your score, or restart quiz
